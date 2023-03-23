c36033b88713c5247e56b9124cb47f4eb7329888
Zen_Shadowborne <sergiofs19@gmail.com>
2023-03-23 15:56:14 +0100

    header css updates

--- index.php
+++ index.php
@@ -6,17 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/general.css">
+    <link rel="stylesheet" href="assets/css/header.css">
 </head>
 <body>
     <header>
         <div class="logo">
-
+            <img src="assets/img/logo.png" alt="logo">
         </div>
         <div class="navegacion">
-
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="index.php">Mecanica</a></li>
+                <li><a href="index.php">Reservas</a></li>
+                <li><a href="index.php">asesoria</a></li>
+            </ul>
         </div>
         <div class="sesion">
-            
+            <ul>
+                <li><a href="index.php">Iniciar Sesion</a></li>
+                <li><a href="index.php">Registrarse</a></li>
+            </ul>
         </div>
     </header>
 </body>
