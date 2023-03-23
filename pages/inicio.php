1b2ded399192137f24c9b5f3de32485407c001d7
Zen_Shadowborne <sergiofs19@gmail.com>
2023-03-23 12:58:54 +0100

    creación de ficheros base

--- pages/inicio.php
+++ pages/inicio.php
@@ -5,19 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/general.css">
 </head>
 <body>
-    <header>
-        <div class="logo">
-
-        </div>
-        <div class="navegacion">
-
-        </div>
-        <div class="sesion">
-            
-        </div>
-    </header>
+    
 </body>
 </html>
