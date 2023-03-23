0aac97e625229f582bc852eb503327ad06529d7e
David Vicario <davidvicario1990@gmail.com>
2023-03-23 16:13:16 +0100

    css y section

--- pages/inicio.php
+++ pages/inicio.php
@@ -5,8 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/inicio_stylo.css">
 </head>
 <body>
-    
+    <main>
+        <section>
+            <h1></h1>
+        </section>
+
+
+        <section>
+            <h2></h2>
+        </section>
+
+        <section>
+            <article>
+
+            </article>
+        </section>
+
+
+    </main>
 </body>
 </html>
