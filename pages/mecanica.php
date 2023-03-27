79cf8aad3da3b95ddccd736c3b0b90ee06f0e723
ingridayala <ingridayala414@gmail.com>
2023-03-27 17:53:15 +0200

    up

--- pages/mecanica.php
+++ pages/mecanica.php
@@ -5,95 +5,340 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/mecanica_style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <title>MECÁNICA DEL AUTOMÓVIL</title>
 </head>
 <body>
-    <h1>Bienvenido a WETAXI MADRID</h1>
-    <p>Somos una empresa de taxis con taller autorizado para revisiones oficiales.</p>
-    <p>Ofrecemos precios especiales para TAXI y tenemos ofertas nuevas cada mes.</p>
-    <p>En nuestro taller experto para todas las marcas de coches, ofrecemos un mantenimiento y reparaciones asequibles y de máxima calidad.</p>
-    <p>Nuestro personal está en formación continua para brindarle el mejor asesoramiento experto en el taller.</p>
-    <p>Además, ofrecemos servicios como:</p>
-<!--Añadir el evento del click (al leer mas ) y quitar el boton -->
-    <section class="toggle-section">
-        <div class="toggle-container">
-            <h2>Neumáticos</h2>
-            <button class="toggle-button">Leer más</button>
-            <div class="toggle-content">
-            <img src="neumaticos.jpg" alt="Neumáticos">
-            <p>En WETAXI MADRID, te ofrecemos una amplia selección de neumáticos de alta calidad para todo tipo de vehículos, siempre al mejor precio.</p>
+    <div class="">
+        <h1>Bienvenido a WETAXI MADRID</h1>
+        <p>Somos una empresa de taxis con taller autorizado para revisiones oficiales.</p>
+        <p>Ofrecemos precios especiales para TAXI y tenemos ofertas nuevas cada mes.</p>
+        <p>En nuestro taller experto para todas las marcas de coches, ofrecemos un mantenimiento y reparaciones asequibles y de máxima calidad.</p>
+        <p>Nuestro personal está en formación continua para brindarle el mejor asesoramiento experto en el taller.</p>
+        <p>Además, ofrecemos servicios como:</p>
+    </div>
+    
+
+    <!--menu acordeon-->
+    <div class="accordion ">
+        <div class="item open">
+            <!--icono rueda es muy pequeño y se ve mal, en busca de otro tire_icon_153938.svg -->
+            <img class="icon"  src="../assets/iconos/tyre_86126.svg" alt="Mi icono">
+            
+            <p class="text">Neumáticos</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            <!--INFORMACION div para cada uno IMG+TESTO+BOTONES -->
+            <div class="hidden-box">
+                <div class="item-contenido">
+                    <div class="img-menu-acordeon">
+                        <img src="../assets/img/neumaticos.jpg" alt="Neumáticos">
+                    </div>
+                
+                    <div class="texto-menu-acordeon">
+                        <p>
+                        En WETAXI MADRID, contamos con un equipo de profesionales que se encargará de revisar y cambiar tus frenos para garantizarte una conducción segura y sin preocupaciones.
+                        </p>
+                        <ul>
+                            <li>TEXTO MACANICA</li>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div class="botones-menu-acordeon" >
+                    <button>saber mas </button>
+                </div>
+                
+            </div>
+        </div>  
+        
+        <div class="item ">
+            <img class="icon" src="../assets/iconos/cleaning_detergent_product_icon_153278.svg" alt="Mi icono">
+            <p class="text">Limpieza interior</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            <div class="hidden-box">
+            <img src="limpieza-coches-03.jpg" alt="Limpieza interior">
+            <p>
+            Nuestro servicio de limpieza interior incluye aspirado y limpieza de tapicería, paneles y cristales para que tu vehículo esté siempre impecable por dentro.
+            </p>
+                <ul>
+                    <li>TEXTO MACANICA</li>
+                    
+                </ul>
+                
+            </div>
+           
+        </div>
+
+        <div class="item ">
+            <img class="icon" src="../assets/iconos/iconfinder-wiper-clean-service-labor-4622491_122432.svg" alt="Mi icono">
+            <p class="text">Limpieza exterior</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            
+            <div class="hidden-box">
+                <img src="limpieza-exterior.jpg" alt="Limpieza exterior">
+                <p>
+                Con nuestro servicio de limpieza exterior, te garantizamos que tu vehículo quedará como el primer día. Limpieza a fondo de carrocería, llantas y cristales para que brille con luz propia.
+                </p>
+                <ul>
+                    <li>TEXTO MACANICA</li>
+                    
+                </ul>
+               
+            </div>
+        </div>   
+
+        <div class="item ">
+            <img class="icon" src="../assets/iconos/brakes_icon_177283.svg" alt="Mi icono">
+            <p class="text">Frenos</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            
+            <div class="hidden-box">
+                <img src="frenos.jpg" alt="Frenos">
+                <p>
+                En WETAXI MADRID, contamos con un equipo de profesionales que se encargará de revisar y cambiar tus frenos para garantizarte una conducción segura y sin preocupaciones.
+                </p>
+                <ul>
+                    <li>TEXTO MACANICA</li>
+                    
+                </ul>
+               
+            </div>
+        </div>   
+        <div class="item ">
+            <img class="icon" src="../assets/iconos/car_icon_145832.svg" alt="Mi icono">
+            <p class="text">Pulido de faros</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            <div class="hidden-box">
+                <img src="pulido-faros.jpg" alt="Pulido de faros">
+                <p>
+                ¿Tus faros han perdido transparencia? En WETAXI MADRID, nos encargamos de restaurarlos para que tu vehículo vuelva a lucir como nuevo y puedas conducir con total seguridad.
+                </p>
+                    <ul>
+                        <li>TEXTO MACANICA</li>
+                        
+                    </ul>
+                    
+            </div>
+           
+        </div>
+        
+       
+        <div class="item ">
+            
+            <img class="icon" src="../assets/iconos/voltmeter_electricity_tool_icon_221330.svg" alt="Mi icono">
+            <p class="text">Electricidad</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            <div class="hidden-box">
+                <img src="electricidad.jpg" alt="Electricidad">
+                <p>
+                ¿Tienes algún problema eléctrico en tu vehículo? En WETAXI MADRID, te ofrecemos un servicio integral de reparación de problemas eléctricos para que no te quedes nunca tirado.
+                </p>
+                    <ul>
+                        <li>TEXTO MACANICA</li>
+                        
+                    </ul>
+                    
+            </div>
+        </div>   
+        <div class="item ">
+            <img class="icon" src="../assets/iconos/screwdriver_wrench_icon_198514.svg" alt="Mi icono">
+            <p class="text">Mantenimiento y revisión</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            
+            <div class="hidden-box">
+            <img src="mantenimineto-integral2.jpg" alt="Mantenimiento y revisión">
+                <p>
+                En WETAXI MADRID, realizamos todo tipo de mantenimiento y revisión de vehículos para que siempre puedas conducir con total seguridad y confianza en tu coche.
+                </p>
+                <ul>
+                    <li>TEXTO MACANICA</li>
+                    
+                </ul>
+            
+            </div>
+            
+        </div>   
+
+        <div class="item ">
+            
+            <img class="icon" src="../assets/iconos/oil_icon_135343.svg" alt="Mi icono">
+            <p class="text">Cambio de aceite y filtros</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
+            </div>
+            <div class="hidden-box">
+            <img src="aceiite-filtro.jpg" alt="Cambio de aceite y filtros">
+                <p>
+                Un buen mantenimiento del motor es imprescindible para el correcto funcionamiento de tu vehículo. En WETAXI MADRID, te ofrecemos el servicio de cambio de aceite y filtros para mantener tu coche en perfectas condiciones
+                </p>
+                <ul>
+                    <li>TEXTO MACANICA</li>
+                    
+                </ul>
+                    
             </div>
         </div>
-    </section>
-
-
-    <section class="toggle-section">
-        <div class="toggle-container">
-            <div class="toggle-bar">
-                <h2>Limpieza interior</h2>
-                <div class="toggle-arrow"></div>
+        <div class="item ">
+            
+        <img class="icon" src="../assets/iconos/fuel_lpg_station_icon_180145.svg" alt="Mi icono">
+            <p class="text">Transformación a GLP</p>
+            <div class="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="icon"
+                    >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                </svg>
             </div>
-            <div class="toggle-content">
-                <img src="limpieza-coches-03.jpg" alt="Limpieza interior">
-                <p>Nuestro servicio de limpieza interior incluye aspirado y limpieza de tapicería, paneles y cristales para que tu vehículo esté siempre impecable por dentro.</p>
+            <div class="hidden-box">
+            <img src="glp.jpg" alt="Transformación a GLP">
+                <p>
+                En WETAXI MADRID tenemos técnicos profesionales que pueden adaptar vehículos de Gasolina a Autogas GLP que incorporen motores de INYECCIÓN INDIRECTA o de INYECCIÓN DIRECTA, tanto para uso particular como profesional que cumplan con los parámetros y requisitos que la normativa española exige para permitir la transformación de vehículos propulsados por Gasolina a Autogas GLP.
+                </p>
+                <ul>
+                    <li>TEXTO MACANICA</li>
+                    
+                </ul>
+                    
             </div>
         </div>
-    </section>
+    </div>
 
-
-    <section>
-    <img src="cuando-cambiar-las-pastillas-de-coche.jpg" alt="Frenos">
-    <h2>Frenos</h2>
-    <p>En WETAXI MADRID, contamos con un equipo de profesionales que se encargará de revisar y cambiar tus frenos para garantizarte una conducción segura y sin preocupaciones.</p>
-    </section>
-
-    <section>
-    <img src="pulido faros.jpg" alt="Pulido de faros">
-    <h2>Pulido de faros</h2>
-    <p>¿Tus faros han perdido transparencia? En WETAXI MADRID, nos encargamos de restaurarlos para que tu vehículo vuelva a lucir como nuevo y puedas conducir con total seguridad.</p>
-    </section>
-
-    <section>
-    <img src="c700x420.jpg" alt="Limpieza exterior">
-    <h2>Limpieza exterior</h2>
-    <p>Con nuestro servicio de limpieza exterior, te garantizamos que tu vehículo quedará como el primer día. Limpieza a fondo de carrocería, llantas y cristales para que brille con luz propia.</p>
-    </section>
-
-    <section>
-    <img src="electricidad.jpg" alt="Electricidad">
-    <h2>Electricidad</h2>
-    <p>¿Tienes algún problema eléctrico en tu vehículo? En WETAXI MADRID, te ofrecemos un servicio integral de reparación de problemas eléctricos para que no te quedes nunca tirado.</p>
-    </section>
-
-    <section>
-    <img src="mantenimineto integral 2.jpg" alt="Mantenimiento y revisión">
-    <h2>Mantenimiento y revisión</h2>
-    <p>En WETAXI MADRID, realizamos todo tipo de mantenimiento y revisión de vehículos para que siempre puedas conducir con total seguridad y confianza en tu coche.</p>
-    </section>
-
-    <section>
-    <img src="images.jpg" alt="Cambio de aceite y filtros">
-    <h2>Cambio de aceite y filtros</h2>
-    <p>Un buen mantenimiento del motor es imprescindible para el correcto funcionamiento de tu vehículo. En WETAXI MADRID, te ofrecemos el servicio de cambio de aceite y filtros para mantener tu coche en perfectas condiciones.</p>
-    </section>
-
-    <p>Contáctenos para obtener más información y reservar su cita en nuestro taller.</p>
-
+    
     <script>
-        const toggleBars = document.querySelectorAll('.toggle-bar');
+        const items = document.querySelectorAll(".item");
 
-        toggleBars.forEach(bar => {
-        bar.addEventListener('click', () => {
-            const container = bar.parentNode;
-            const content = container.querySelector('.toggle-content');
-            const arrow = bar.querySelector('.toggle-arrow');
-
-            container.classList.toggle('active');
-            content.classList.toggle('active');
-            arrow.classList.toggle('active');
+        for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener("click", function () {
+            this.classList.toggle("open");
         });
-        });
-
+        }
     </script>
     
 </body>
