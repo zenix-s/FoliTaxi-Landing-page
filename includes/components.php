e6c78ec5cca4a82f901c10c46c9ba49fc02cec66
Zen_Shadowborne <sergiofs19@gmail.com>
2023-03-27 12:29:30 +0200

    añadido footer

--- includes/components.php
+++ includes/components.php
@@ -13,10 +13,10 @@ class componentes
             </div>
             <div class="navegacion">
                 <ul>
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="index.php">Mecanica</a></li>
-                    <li><a href="index.php">Reservas</a></li>
-                    <li><a href="index.php">asesoria</a></li>
+                    <li><a href="inicio.php">Inicio</a></li>
+                    <li><a href="mecanica.php">Mecanica</a></li>
+                    <li><a href="reservas.php">Reservas</a></li>
+                    <li><a href="asesoria.php">asesoria</a></li>
                 </ul>
             </div>
             <div class="sesion">
@@ -26,6 +26,38 @@ class componentes
                 </ul>
             </div>
             </header>
+        ';
+    }
+
+    // Función encargada de generar el footer de la página
+    function footer()
+    {
+        return '
+        <footer>
+        <div class="footer_container">
+            <div class="logo">
+                <img src="../assets/img/logo.png" alt="logo">
+            </div>
+            <div class="redes">
+                <ul>
+                    <li><a href="index.php"><i class="bi bi-facebook"></i></a></li>
+                    <li><a href="index.php"><i class="bi bi-facebook"></i></a></li>
+                    <li><a href="index.php"><i class="bi bi-instagram"></i></a></li>
+                    <li><a href="index.php"><i class="bi bi-twitter"></i></a></li>
+                    <li><a href="index.php"><i class="bi bi-youtube"></i></a></li>
+                    <li><a href="index.php"><i class="bi bi-tiktok"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav_options">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="index.php">Mecanica</a></li>
+                <li><a href="index.php">Reservas</a></li>
+                <li><a href="index.php">asesoria</a></li>
+            </ul>
+        </div>
+        </footer>
         ';
     }
 }
