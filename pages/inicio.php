cb518fe776860d1e33e3d7ed9f462862bba7d9a7
Zen_Shadowborne <sergiofs19@gmail.com>
2023-03-27 11:05:43 +0200

    servicios secction inicio

--- pages/inicio.php
+++ pages/inicio.php
@@ -6,27 +6,87 @@ $componentes = new componentes();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/inicio_stylo.css">
+    <link rel="stylesheet" href="../assets/css/inicio_style.css">
     <link rel="stylesheet" href="../assets/css/header.css">
 </head>
+
 <body>
     <?php
-        echo $componentes->header();
+    echo $componentes->header();
     ?>
     <main>
-        <section class="primeraSeccion">
-            <div class="textoUno">
-                <h1 class="titulo">LUNES A VIERNES DE 09:00 - 19:00</h1>    
-                <h2>ASISTENCIA URGENCIAS 24 HORAS</h2>
+        <section class="slider">
+
+        </section>
+
+        <section class="servicios-section">
+            <h2>Servicios</h2>
+            <div class="services-container">
+                <div class="service-container">
+                    <div class="service">
+                        <div class="service-img">
+                            <img src="../assets/img/taller-taxis-.jpg" alt="escuela">
+                        </div>
+                        <div class="service-description">
+                            <h3>Escuela de conductores</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                            <a href="#">Saber mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="service-container">
+
+
+                    <div class="service">
+                        <div class="service-img">
+                            <img src="../assets/img/taxis.png" alt="bucle">
+                        </div>
+                        <div class="service-description">
+                            <h3>Bucle magnetico</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                            <a href="#">Saber mas</a>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="service-container">
+                    <div class="service">
+                        <div class="service-img">
+                            <img src="../assets/img/taxis.png" alt="asesoria">
+                        </div>
+                        <div class="service-description">
+                            <h3>Asesoria</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                            <a href="#">Saber mas</a>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="service-container">
+                    <div class="service">
+                        <div class="service-img">
+                            <img src="../assets/img/taxis.png" alt="taller">
+                        </div>
+                        <div class="service-description">
+                            <h3>Taller</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                            <a href="#">Saber mas</a>
+
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </section>
 
 
     </main>
 </body>
+
 </html>
