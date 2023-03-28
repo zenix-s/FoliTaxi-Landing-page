278cb552ced08ceda0f238eb6dff54518ed3e6b0
Zen_Shadowborne <sergiofs19@gmail.com>
2023-03-28 10:11:52 +0200

    header updates

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,10 +10,75 @@
     <link rel="stylesheet" href="assets/css/header.css">
     <link rel="stylesheet" href="assets/css/footer.css">
     <!-- icons -->
-    <link rel="stylesheet" href="assets/icons/bootstrap-icons.css"> 
+    <link rel="stylesheet" href="assets/icons/bootstrap-icons.css">
 </head>
+
 <body>
 
-    
+    <header>
+        <div class="logo">
+            <img src="assets/img/logo.png" alt="logo">
+        </div>
+        <div class="navegacion">
+            <ul>
+                <li><a href="inicio.php">Inicio</a></li>
+                <li><a href="mecanica.php">Mecanica</a></li>
+                <li><a href="reservas.php">Reservas</a></li>
+                <li><a href="asesoria.php">asesoria</a></li>
+            </ul>
+        </div>
+        <div class="sesion">
+            <ul>
+                <li><a href="index.php">Iniciar Sesion</a></li>
+                <li><a href="index.php">Registrarse</a></li>
+            </ul>
+        </div>
+        <div class="mobile-close-header">
+            <i class="bi bi-list"></i>
+        </div>
+    </header>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-subsection">
+                <div class="logo-container">
+                    <img src="assets/img/logo.png" alt="logo">
+                </div>
+                <nav class="social-networks-container">
+                    <div class="social-network-item">
+                        <a href="#">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                    </div>
+                    <div class="social-network-item">
+                        <a href="#">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                    </div>
+                    <div class="social-network-item">
+                        <a href="#">
+                            <i class="bi bi-twitter"></i>
+                        </a>
+                    </div>
+                    <div class="social-network-item">
+                        <a href="#">
+                            <i class="bi bi-youtube"></i>
+                        </a>
+                    </div>
+                    <div class="social-network-item">
+                        <a href="#">
+                            <i class="bi bi-tiktok"></i>
+                        </a>
+                    </div>
+                </nav>
+            </div>
+            <div class="footer-subsection">
+                
+            </div>
+            <div class="footer-subsection">
+
+            </div>
+        </div>
+    </footer>
 </body>
+
 </html>
